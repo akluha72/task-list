37e473dbff281e02fe270ebcbed08b7a9fed8272
lutfi_akmal <lutfiakmal@myra.com.my>
2024-08-01 11:57:03 +0800

    Route configuration

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return 'Main Page';
+})->name('my route');
+
+
+Route::get('/hello', function(){
+    return "you're back in the main page";
+})->name('hello');
+
+//redirect url
+Route::get('/redirect', function(){
+    return redirect('hello');
 });
+
+
+//For not listed url, it will redirect to this route. 
+Route::fallback(function(){
+    return ' where are you going actually huh?, this page has been return by fallback route';
+});
+
